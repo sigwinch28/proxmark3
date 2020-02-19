597b6669e874d547d1dcfac90509fac945b6121b
Joe Harrison <joe@sigwinch.uk>
2020-02-19 16:10:26 +0000

    Add script interpreter tests to pm3test.sh
    To check that our shebangs in pm3, client, and tools are portable
    enough we try to invoke each of the scripts and check whether the
    return code is not 126 or 127, which indicates that bash cannot find
    the interpreter in the former case, and that env from gnu coreutils
    cannot find the command in the latter case.

--- pm3test.sh
+++ pm3test.sh
@@ -62,6 +62,25 @@ function CheckExecute() {
   return 1
 }
 
+# Checks whether a given file's interpreter can be found by attempting to exec
+# the file and checking whether the return code doesn't indicate that:
+# - the interpreter couldn't be found; or
+# - `env` couldn't find the command
+function CheckInvoke() {
+  RES=$(eval "$2" 2>&1)
+  RET=$?
+  # bash returns 126 if interpreter not found
+  # env (in GNU coreutils) returns 127 if command not found
+  if [ $RET -ne 126 ] && [ $RET -ne 127 ]; then
+    echo -e "$1 ${C_GREEN}[OK]${C_NC}"
+    return 0
+  fi
+
+  echo -e "$1 ${C_RED}[Fail]${C_NC}"
+  echo -e "Execution trace:\n$RES"
+  return 1
+}
+
 printf "\n${C_BLUE}RRG/Iceman Proxmark3 test tool ${C_NC}\n\n"
 
 printf "work directory: "
@@ -87,6 +106,28 @@ while true; do
   if ! CheckFileExist "arm image exists" "./armsrc/obj/fullimage.elf"; then break; fi
   if ! CheckFileExist "bootrom exists" "./bootrom/obj/bootrom.elf"; then break; fi
   if ! CheckFileExist "hardnested tables exists" "./client/resources/hardnested_tables/*.z"; then break; fi
+
+  printf "\n${C_BLUE}Testing interpreters for executable scripts:${C_NC}\n"
+  if ! CheckInvoke "pm3 interpreter" "./pm3 -h"; then break; fi
+  if ! CheckInvoke "pm3-flash interpreter" "./pm3-flash -h"; then break; fi
+  if ! CheckInvoke "pm3-flash-all interpreter" "./pm3-flash-all -h"; then break; fi
+  if ! CheckInvoke "pm3-flash-bootrom interpreter" "./pm3-flash-bootrom -h"; then break; fi
+  if ! CheckInvoke "pm3-flash-fullimage interpreter" "./pm3-flash-fullimage -h"; then break; fi
+  if ! CheckInvoke "client/proxmark3 interpreter" "./client/proxmark3 -h"; then break; fi
+  if ! CheckInvoke "tools/analyzesize.py interpreter" "./tools/analyzesize.py -h"; then break; fi
+  if ! CheckInvoke "tools/findbits.py interpreter" "./tools/findbits.py"; then break; fi
+  if ! CheckInvoke "tools/mkversion.sh interpreter" "./tools/mkversion.sh"; then break; fi
+  if ! CheckInvoke "tools/pm3_amii_bin2eml.pl interpeter" "tools/pm3_amii_bin2eml.pl"; then break; fi
+  if ! CheckInvoke "tools/pm3_cs8.pl interpreter" "./tools/pm3_cs8.pl"; then break; fi
+  if ! CheckInvoke "tools/pm3_eml2lower.sh interpreter" "./tools/pm3_eml2lower.sh"; then break; fi
+  if ! CheckInvoke "tools/pm3_eml2mfd.py interpreter" "./tools/pm3_eml2mfd.py"; then break; fi
+  if ! CheckInvoke "tools/pm3_eml2upper.sh interpreter" "./tools/pm3_eml2upper.sh"; then break; fi
+  if ! CheckInvoke "tools/pm3_eml_mfd_test.py interpreter" "./tools/pm3_eml_mfd_test.py"; then break; fi
+  if ! CheckInvoke "tools/pm3_mf7b_wipe.py interpreter" "./tools/pm3_mf7b_wipe.py"; then break; fi
+  if ! CheckInvoke "tools/pm3_mfd2eml.py interpreter" "./tools/pm3_mfd2eml.py"; then break; fi
+  if ! CheckInvoke "tools/pm3_mfdread.py interpreter" "./tools/pm3_mfdread.py"; then break; fi
+  if ! CheckInvoke "tools/pm3_pm32wav.py interpreter" "./tools/pm3_pm32wav.py"; then break; fi
+  if ! CheckInvoke "tools/xorcheck.py interpeter" "./tools/xorcheck.py"; then break; fi
 
   printf "\n${C_BLUE}Testing basic help:${C_NC}\n"
   if ! CheckExecute "proxmark help" "./client/proxmark3 -h" "wait"; then break; fi
@@ -116,6 +157,7 @@ while true; do
   if ! CheckExecute "mfkey64 test" "tools/mfkey/mfkey64 9c599b32 82a4166c a1e458ce 6eea41e0 5cadf439" "Found Key: \[ffffffffffff\]"; then break; fi
   if ! CheckExecute "mfkey64 long trace test" "tools/mfkey/./mfkey64 14579f69 ce844261 f8049ccb 0525c84f 9431cc40 7093df99 9972428ce2e8523f456b99c831e769dced09 8ca6827b ab797fd369e8b93a86776b40dae3ef686efd c3c381ba 49e2c9def4868d1777670e584c27230286f4 fbdcd7c1 4abd964b07d3563aa066ed0a2eac7f6312bf 9f9149ea" "Found Key: \[091e639cb715\]"; then break; fi
   if ! CheckExecute "nonce2key test" "tools/nonce2key/nonce2key e9cadd9c a8bf4a12 a020a8285858b090 050f010607060e07 5693be6c00000000" "key recovered: fc00018778f7"; then break; fi
+
   printf "\n${C_GREEN}Tests [OK]${C_NC}\n\n"
   exit 0
 done
